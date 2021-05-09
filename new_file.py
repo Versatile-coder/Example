0e6edefe936daf1ac0dac8cf11641a211114fabc
Vinayaka_Hegde <V_Hegde@Dell.com>
2021-05-09 16:54:53 +0530

    added new_file.py

---
+++ new_file.py
@@ -0,0 +1,12 @@
+ Volume in drive C is OS
+ Volume Serial Number is 48A0-2AE9
+
+ Directory of C:\Jenkin_Git
+
+05/09/2021  04:54 PM    <DIR>          .
+05/09/2021  04:54 PM    <DIR>          ..
+05/09/2021  03:50 PM                33 Example.py
+05/09/2021  04:50 PM               386 extra_file.txt
+05/09/2021  04:54 PM                 0 new_file.py
+               3 File(s)            419 bytes
+               2 Dir(s)  307,324,616,704 bytes free
